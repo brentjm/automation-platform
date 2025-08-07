65b8c25a03f27da87e0953d4ff5ea46f5bf75f34
brentjm <brent_maranzano@yahoo.com>
2025-08-07 12:01:28 -0400

    Moving task specific parameters and meta data from specific columns in the Task table to a JSON blob stored in the table, as most of this doesn't need to be searchable.

--- app/backend/src/laf/models.py
+++ app/backend/src/laf/models.py
@@ -44,22 +44,31 @@ class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     workflow_id = db.Column(db.Integer, db.ForeignKey("workflow.id"), nullable=False)
     name = db.Column(db.String(128), nullable=False)
-    instrument = db.Column(db.String(128), nullable=False)  # e.g., 'instrument-a'
-    status = db.Column(
-        db.String(64), default="pending"
-    )  # pending, running, completed, failed
-    order_index = db.Column(db.Integer, default=0)  # To maintain task order
-    created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
-    updated_at = db.Column(
-        db.DateTime, default=datetime.datetime.utcnow, onupdate=datetime.datetime.utcnow
+    status = db.Column(db.String(64), default="pending")
+    order_index = db.Column(db.Integer, default=0)
+    executed_at = db.Column(
+        db.DateTime, default=datetime.datetime.now(datetime.timezone.utc)
     )
+    # Results should mostly be a pointer to data stored in an external system
+    # keeping with the idea of data fabric and not duplicating data.
     results = db.relationship(
         "Result", backref="task", lazy=True, cascade="all, delete-orphan"
     )
+    # The actual metadata and instructions should be separate, as some
+    # will be stored in external systems like ELN or LIMS. In any case,
+    # users should be able to create new tasks without this model needing
+    # updated. Moreover, the task will be carried out by a Kubernetes
+    # Jobs or Celery, so the task model should not be tied to a specific
+    # implementation.
+    recipe = db.Column(JSONB)  # Stores task parameters and instructions
+    recipe_type = db.Column(
+        db.String(64)
+    )  # Used for filtering/search (e.g. "Biovia Onelab")
+    recipe_hash = db.Column(db.String(64), unique=True)  # Hash of task recipe
 
 
 class Result(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     task_id = db.Column(db.Integer, db.ForeignKey("task.id"), nullable=False)
-    data = db.Column(JSONB)
     created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+    data = db.Column(JSONB)
